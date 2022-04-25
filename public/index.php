312bb364264d767fc3193b9f5383a47d40e8d47f
Mary <regentmarysapro@gmail>
2022-04-25 17:43:10 +0200

    add correction

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+
+require '../vendor/autoload.php';
+
 use App\Wcs\Hello;
 
-include __DIR__ . './../vendor/autoload.php';
 $hello = new Hello();
-echo $hello->text();
+echo $hello->talk();
 
+var_dump($hello);
--- src/Wcs/Hello.php
+++ src/Wcs/Hello.php
@@ -1,29 +1,11 @@
 <?php
-namespace App\Wcs\Hello;
 
-class Hello
+namespace App\Wcs;
+
+class Hello 
 {
-    public $hello = "Hello World !";
+  public function talk()
+  {
+    return "Hello world !";
+  }
 }
-
-include __DIR__ . './../vendor/autoload.php';
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <h1>Hello world !</h1>
-</body>
-
-</html>
